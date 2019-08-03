44a03b357ae1fa3d3827e967187509fec4fe45bc
melan96 <it17006712@my.sliit.lk>
2019-08-03 06:04:10 -0700

    ordb table creation using defined multiple sql objs types Added to sqlBase

--- CreateTYPE.sql
+++ CreateTYPE.sql
@@ -3,8 +3,13 @@
 CREATE TYPE student_ty AS OBJECT(
     name VARCHAR2(50),
     studentID int
-)
+);
 /
+
+CREATE TYPE course_ty AS OBJECT (
+    courseName VARCHAR2(20),
+    courseID INT
+);
 
 --Create table by object type each row contains details of student_ty 
 
@@ -19,4 +24,22 @@ INSERT INTO student_tab values (
 );
 
 /
-select * from student_tab
+
+
+CREATE TABLE student_course_table (
+
+    course course_ty,
+    student student_ty
+);
+
+/
+
+INSERT INTO student_course_table VALUES (
+
+    course_ty('SPM',25412),
+    student_ty('melan96',257840)
+);
+
+/
+
+select * from student_course_table;
